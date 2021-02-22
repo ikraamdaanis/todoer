63fdeacd1fcc7890018fa72de035c9223cea6b77
Ikraam <IbnDaanis@Gmail.com>
2021-02-22 15:09:15 +0000

    Added components, pages, containers and React Router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ScrollToTop } from './components'
+import { Navbar } from './containers'
+import { Home, SignIn, SignUp } from './pages'
+import { GlobalStyle } from './styles/globalStyle'
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Navbar />
+        <Switch>
+          <ScrollToTop />
+          <Route exact path='/' render={Home} />
+          <Route exact path='/signin' render={SignIn} />
+          <Route exact path='/signup' render={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { Home } from './Home/Home'
+export { SignIn } from './SignIn/SignIn'
+export { SignUp } from './SignUp/SignUp'
