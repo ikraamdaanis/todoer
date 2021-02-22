47b3ae7813e7b570cb80aed5cb132d78abeb08dd
Ikraam <IbnDaanis@Gmail.com>
2021-02-22 16:10:30 +0000

    Added to navbar

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import {
+  RightContainer,
+  Container,
+  NavbarContainer,
+  AuthButton,
+} from './NavbarStyles'
+import { ReactComponent as MenuToggler } from '../../assets/images/menu-toggler.svg'
+import { ReactComponent as HomeIcon } from '../../assets/images/home-icon.svg'
 
 export const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <header>
+      <NavbarContainer>
+        <Container>
+          <MenuToggler className='menu-toggler' />
+          <HomeIcon className='home' />
+          <RightContainer>
+            <AuthButton>Sign In</AuthButton>
+            <AuthButton>Sign Out</AuthButton>
+          </RightContainer>
+        </Container>
+      </NavbarContainer>
+    </header>
+  )
 }
--- src/containers/Navbar/NavbarStyles.jsx
+++ src/containers/Navbar/NavbarStyles.jsx
@@ -1,1 +1,67 @@
 import styled from 'styled-components/macro'
+
+export const NavbarContainer = styled.nav`
+  background: var(--dark-gray);
+  height: 44px;
+  border-bottom: 1px solid transparent;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.15);
+  position: relative;
+  z-index: 1000;
+`
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0 42px 0;
+
+  @media (max-width: 550px) {
+    padding: 0 10px 0;
+  }
+
+  .menu-toggler {
+    cursor: pointer;
+    transition: 0.2s ease-in-out;
+    &:hover {
+      @media (min-width: 600px) {
+        color: var(--light-red);
+      }
+    }
+  }
+
+  .home {
+    cursor: pointer;
+    transition: 0.2s ease-in-out;
+    &:hover {
+      @media (min-width: 600px) {
+        color: var(--light-red);
+      }
+    }
+  }
+`
+
+export const RightContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  button {
+    margin-left: 0.5rem;
+  }
+`
+export const AuthButton = styled.button`
+  display: block;
+  background: var(--light-red);
+  border: 0;
+  outline: 0;
+  color: var(--text-white);
+  border-radius: 5px;
+  padding: 4px 8px 5px;
+  line-height: 100%;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    filter: brightness(1.1);
+  }
+`
