6f25d8d8c0ff6315544e7f40d6201526ee87c519
Ikraam <IbnDaanis@Gmail.com>
2021-02-22 19:35:32 +0000

    Added firebase hooks

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -10,13 +10,16 @@ import { ReactComponent as HomeIcon } from '../../assets/images/home-icon.svg'
 import { auth, firestore } from '../../firebase/config'
 import firebase from 'firebase/app'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
 
 export const Navbar = () => {
   const [user] = useAuthState(auth)
+  const [todos] = useCollection(firebase.firestore().collection('users'))
 
   useEffect(() => {
-    user ? console.log(user.uid) : console.log(user)
-  }, [user])
+    todos?.docs.forEach(doc => console.log(doc.data()))
+  }, [user, todos])
+
   return (
     <header>
       <NavbarContainer>
