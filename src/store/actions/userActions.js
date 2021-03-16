52c88308d1711753891ee242e498844ab70e2c8b
Ikraam <IbnDaanis@Gmail.com>
2021-03-16 16:14:40 +0000

    refactored register action

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -18,56 +18,59 @@ export const registerAction = (googleSignIn, email, password) => async dispatch 
       type: USER_REGISTER_REQUEST,
     })
 
-    const user = {}
+    const registerSuccess = async user => {
+      await firestore.collection('users').doc(auth.currentUser.uid).set(user)
+
+      await firestore.collection('users').doc(user.id).collection('projects').doc('Inbox').set({
+        title: 'Inbox',
+        createdAt: new Date(),
+        id: uuidv4(),
+      })
+
+      dispatch({
+        type: USER_REGISTER_SUCCESS,
+        payload: user,
+      })
+
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: user,
+      })
+
+      localStorage.setItem('userInfo', JSON.stringify(user))
+    }
 
     if (googleSignIn) {
       const provider = new firebase.auth.GoogleAuthProvider()
       await auth.signInWithPopup(provider).then(response => {
-        user.id = response.user.uid
-        user.name = response.user.displayName
-        user.email = response.user.email
-        user.photo = response.user.photoURL
-        user.createdAt = new Date()
+        registerSuccess({
+          id: response.user.uid,
+          name: response.user.displayName,
+          email: response.user.email,
+          photo: response.user.photoURL,
+          createdAt: new Date(),
+        })
       })
     } else {
       await firebase
         .auth()
         .createUserWithEmailAndPassword(email, password)
         .then(userCredential => {
-          user.id = userCredential.user.uid
-          user.name = userCredential.user.email.split('@')[0]
-          user.email = userCredential.user.email
-          user.photo = 'https://i.stack.imgur.com/34AD2.jpg'
-          user.createdAt = new Date()
+          registerSuccess({
+            id: userCredential.user.uid,
+            name: userCredential.user.email.split('@')[0],
+            email: userCredential.user.email,
+            photo: 'https://i.stack.imgur.com/34AD2.jpg',
+            createdAt: new Date(),
+          })
         })
         .catch(error => {
-          console.log(error.message)
           dispatch({
             type: USER_REGISTER_FAIL,
             payload: error.message,
           })
         })
     }
-
-    await firestore.collection('users').doc(auth.currentUser.uid).set(user)
-
-    await firestore.collection('users').doc(user.id).collection('projects').doc('Inbox').set({
-      title: 'Inbox',
-      createdAt: new Date(),
-      id: uuidv4(),
-    })
-
-    dispatch({
-      type: USER_REGISTER_SUCCESS,
-      payload: user,
-    })
-
-    dispatch({
-      type: USER_LOGIN_SUCCESS,
-      payload: user,
-    })
-
-    localStorage.setItem('userInfo', JSON.stringify(user))
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
