76965e33c28e403a05cd8827d4837b3dfbee6b42
Ikraam <IbnDaanis@Gmail.com>
2021-03-29 22:58:55 +0100

    Added use callback to useSetPosition

--- src/hooks/useSetPosition.jsx
+++ src/hooks/useSetPosition.jsx
@@ -1,22 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 export const useSetPosition = (buttonRef, isClosed) => {
   const [position, setPosition] = useState(0)
   const buttonPosition = buttonRef?.current?.getBoundingClientRect().right
 
-  const positionMenu = () => {
+  const positionMenu = useCallback(() => {
     if (buttonPosition + 125 < window.innerWidth) {
       setPosition(buttonPosition - 150)
     } else {
       setPosition(window.innerWidth - 250)
     }
-  }
+  }, [buttonPosition])
 
   useEffect(() => {
     positionMenu()
 
     window.addEventListener('resize', positionMenu)
-  }, [buttonPosition, isClosed])
+  }, [buttonPosition, isClosed, positionMenu])
 
   return [position]
 }
